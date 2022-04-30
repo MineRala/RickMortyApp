ade654938e94bc3e7a09a5fc22afa9b328dc8965
Mine Rala <mine-rala@hotmail.com>
2022-04-30 17:31:22 +0300

    fetchAllDatas function implemented

--- RickMortyApp/Model/RickMortyModel.swift
+++ RickMortyApp/Model/RickMortyModel.swift
@@ -8,20 +8,33 @@
 import Foundation
 
 //MARK: - PostModel
-struct PostModel {
+struct PostModel: Codable {
     let info: Info?
     let results: [Result]?
+    
+    enum CodingKeys: String, CodingKey {
+        case info
+        case results
+    }
+    
+    // Codable yapınca enum hallerini de eklemek gerekiyor.
 }
 
 //MARK: - Info
-struct Info {
+struct Info: Codable {
     let count: Int?
     let pages: Int?
     let next: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case count
+        case pages
+        case next
+    }
 }
 
 //MARK: - Result
-struct Result {
+struct Result: Codable {
     let id: Int?
     let name: String?
     let status: String?
@@ -34,10 +47,30 @@ struct Result {
     let episode: [String]?
     let url: String?
     let created: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case status
+        case species
+        case type
+        case gender
+        case origin
+        case location
+        case image
+        case episode
+        case url
+        case created
+    }
 }
 
 //MARK: - Location
-struct Location {
+struct Location: Codable {
     let name: String?
     let url: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case url
+    }
 }
--- RickMortyApp/Service/RickMortyService.swift
+++ RickMortyApp/Service/RickMortyService.swift
@@ -11,15 +11,29 @@ import Alamofire
 enum RickMortyServiceEndPoint: String {
     case BASE_URL = "https://rickandmortyapi.com/api"
     case PATH = "/character"
+    
+    static func characterPath() -> String {
+        return "\(BASE_URL.rawValue)\(PATH.rawValue)"
+    }
 }
 
 protocol IRickMortyService {
-    func fetchAllDatas()
+    func fetchAllDatas(response: @escaping ([Result]?) -> Void)
 }
 
+// Datayı geri verebilmek için callback function olarak geri verebilmek için @escaping ekliyorum.
 struct RickMortyService: IRickMortyService {
-    func fetchAllDatas() {
-        
+    func fetchAllDatas(response: @escaping ([Result]?) -> Void) {
+        // responseDecodable yerine responseJSON kullanırsam hem error'ü de handle edebilirim.
+        AF.request(RickMortyServiceEndPoint.characterPath()).responseDecodable(of: PostModel.self) { (model) in
+            guard let data = model.value else {
+                response(nil)
+                return
+            }
+            // PostModel olarak gelecek kullanıcıya Result'ı göstericem.
+            response(data.results)
+            
+        }
     }
     
     
